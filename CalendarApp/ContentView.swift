8e17c7a2c19b8ccbf1b63c8d33bb3f464c528c98
Atabany <mohamed.elatabany@leanscale.com>
2023-09-28 01:12:14 +0400

    first

--- CalendarApp/ContentView.swift
+++ CalendarApp/ContentView.swift
@@ -1,88 +1,160 @@
-//
-//  ContentView.swift
-//  CalendarApp
-//
-//  Created by Mohamed Elatabany on 28/09/2023.
-//
-
+import Foundation
 import SwiftUI
-import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+struct CalendarView: View {
+    let calendar = Calendar.current
+    
+    let daysOfWeek = Array(Calendar.current.shortWeekdaySymbols[1...]) + [Calendar.current.shortWeekdaySymbols.first!]
+    
+    @State private var selectedDate: Date = Date()
+    @State private var monthOffset: Int = 0
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack(alignment: .center, spacing: 10) {
+            // Month and Year Navigation
+            HStack {
+                Button(action: {
+                    withAnimation {
+                        monthOffset -= 1
+                    }
+                }) {
+                    Image(systemName: "chevron.left")
+                }
+                
+                Spacer()
+                
+                Text("\(monthFormatter.string(from: currentMonth)) \(yearFormatter.string(from: currentMonth))")
+                    .font(.title)
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation {
+                        monthOffset += 1
+                    }
+                }) {
+                    Image(systemName: "chevron.right")
+                }
+            }
+            
+            Divider()
+            
+            // Weekdays Header
+            HStack {
+                ForEach(daysOfWeek, id: \.self) { day in
+                    Text(day)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            // Days of the month
+            ForEach(0..<6) { weekRow in
+                HStack {
+                    ForEach(0..<7) { dayColumn in
+                        if startingWeekday <= dayColumn + 1 || weekRow > 0, let day = dayOfMonth(for: weekRow, dayColumn) {
+                            Button(action: {
+                                print("Year: \(year), Month: \(month), Day: \(day)")
+                                var components = DateComponents(year: year, month: month, day: day)
+                                components.calendar = Calendar.current
+                                if let selected = components.date {
+                                    selectedDate = selected
+                                } else {
+                                    print("Unable to construct date")
+                                }
+                            }) {
+                                Text("\(day)")
+                                    .frame(width: 40, height: 40)
+                                    .background(backgroundColor(for: day))
+                                    .foregroundColor(textColor(for: day))
+                                    .clipShape(Circle())
+                            }
+                        } else {
+                            Text("")
+                                .frame(width: 40, height: 40)
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
+        .padding()
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    
+    func backgroundColor(for day: Int) -> Color {
+        
+        var components = DateComponents(year: year, month: month, day: day)
+        components.calendar = Calendar.current
+        
+        guard let date = components.date else {
+            return Color.clear
+        }
+        
+        if calendar.isDateInToday(date) {
+            return Color.red
+        } else if calendar.isDate(selectedDate, inSameDayAs: date) {
+            return Color.blue
+        } else {
+            return Color.clear
+        }
+    }
+    
+    func textColor(for day: Int) -> Color {
+        
+        var components = DateComponents(year: year, month: month, day: day)
+        components.calendar = Calendar.current
+        
+        guard let date = components.date else {
+            return Color.black
+        }
+        
+        
+        if calendar.isDateInToday(date) || calendar.isDate(selectedDate, inSameDayAs: date) {
+            return Color.white
+        } else {
+            return Color.black
+        }
+    }
+    
+    // Computed Properties
+    var currentMonth: Date {
+        Calendar.current.date(byAdding: .month, value: monthOffset, to: Date()) ?? Date()
+    }
+    
+    var month: Int {
+        Calendar.current.component(.month, from: currentMonth)
+    }
+    
+    var year: Int {
+        Calendar.current.component(.year, from: currentMonth)
+    }
+    
+    var daysInMonth: Int {
+        calendar.range(of: .day, in: .month, for: currentMonth)!.count
+    }
+    
+    var startingWeekday: Int {
+        return calendar.component(.weekday, from: currentMonth)
+    }
+    
+    func dayOfMonth(for weekRow: Int, _ dayColumn: Int) -> Int? {
+        let day = (weekRow * 7 + dayColumn + 1) - startingWeekday + 1
+        return day >= 1 && day <= daysInMonth ? day : nil
+    }
+    
+    func isDateToday(_ day: Int) -> Bool {
+        var components = DateComponents(year: year, month: month, day: day)
+        components.calendar = calendar
+        if let date = components.date {
+            return calendar.isDateInToday(date)
+        } else {
+            return false
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
+struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        CalendarView()
     }
 }
+
